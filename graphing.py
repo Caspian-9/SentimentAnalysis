d558b850586cfaa09ba778094fb849e6c9ef4277
SlothfulAlfred <75184429+SlothfulAlfred@users.noreply.github.com>
2021-12-11 22:05:56 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -1,6 +1,10 @@
-import plotly.graph_objects as go
+"""
+Graphs our data using plotly.graph_objects and writes to a png image
+using kaleido.
+"""
 import datetime
 import csv
+import plotly.graph_objects as go
 import filtration as f
 
 # Dependencies:
@@ -9,13 +13,22 @@ import filtration as f
 # - csv
 # - datetime
 
-cbc = f.store_cbc_to_dataclass('datasets/articles.json')
-global_ = f.store_global_to_dataclass('datasets/global.json')
-star = f.store_star_to_dataclass('datasets/the_star.json')
+# Global constants. These never change, and we really don't want to call
+# store_cbc_to_dataclass more than once since it is very, very slow.
+PUBLISH_DATES = [
+    datetime.datetime(2020, 7, 14),
+    datetime.datetime(2020, 11, 13),
+    datetime.datetime(2021, 3, 5),
+    datetime.datetime(2021, 5, 28),
+    datetime.datetime(2021, 8, 27)
+]
+
+CBC = f.store_cbc_to_dataclass('datasets/articles.json')
+GLOBAL = f.store_global_to_dataclass('datasets/global.json')
+STAR = f.store_star_to_dataclass('datasets/the_star.json')
 
 
-# TODO: Add type annotation [int, int]
-def get_avg_polarity_for_day(day: datetime.datetime, data: f.FilteredDataset) -> tuple:
+def get_avg_polarity_for_day(day: datetime.datetime, data: f.FilteredDataset) -> tuple[int, int]:
     """
     Returns number of positive articles and number of articles in data that were
     published on a given day.
@@ -33,8 +46,7 @@ def get_avg_polarity_for_day(day: datetime.datetime, data: f.FilteredDataset) ->
     return pos_articles, len(articles)
 
 
-# TODO: add type annotation list[list[float]]
-def read_data(file: str) -> list:
+def read_data(file: str) -> list[list[float]]:
     """
     Reads the csv file and returns a list of lists. Each list contained represents
     the time until the company goes bankrupt, and each inner list is sorted by
@@ -70,15 +82,6 @@ def read_data(file: str) -> list:
     return filtered_data
 
 
-publish_dates = [
-    datetime.datetime(2020, 7, 14),
-    datetime.datetime(2020, 11, 13),
-    datetime.datetime(2021, 3, 5),
-    datetime.datetime(2021, 5, 28),
-    datetime.datetime(2021, 8, 27)
-]
-
-
 def get_percentage_for_quarter(quarter: int) -> float:
     """
     Returns the percentage of positive articles published during the given quarter.
@@ -88,11 +91,11 @@ def get_percentage_for_quarter(quarter: int) -> float:
     """
     total_pos = 0
     total_articles = 0
-    start_date = publish_dates[quarter]
-    for i in range((start_date - publish_dates[quarter - 1]).days):
-        global_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), global_)
-        star_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), star)
-        cbc_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), cbc)
+    start_date = PUBLISH_DATES[quarter]
+    for i in range((start_date - PUBLISH_DATES[quarter - 1]).days):
+        global_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), GLOBAL)
+        star_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), STAR)
+        cbc_articles = get_avg_polarity_for_day(start_date + datetime.timedelta(i), CBC)
 
         day_positive = global_articles[0] + star_articles[0] + cbc_articles[0]
         day_articles = global_articles[1] + star_articles[1] + cbc_articles[1]
@@ -102,36 +105,81 @@ def get_percentage_for_quarter(quarter: int) -> float:
     return total_pos / total_articles * 100
 
 
-fig = go.Figure()
+def generate_graph(months_until: int) -> None:
+    """
+    Generates a double bar graph with percentage of positive articles
+    and percentage of businesses that will go bankrupt in a given time.
+    The graph will be saved "graph{months_until}.png". For example, if
+    months_until is 3, then the graph will be saved as "graph3.png".
 
-fig.add_trace(
-    go.Bar(
-        x=publish_dates[1:],
-        y=[get_percentage_for_quarter(i) for i in range(1, 5)],
-        name='%age of positive articles'
-    ))
+    You can adjust the time until bankruptcy with the months_until parameter.
+    Here are the accepted values and their effects:
 
-bankruptcy_data = [
-    read_data('datasets/july_2020.csv'),
-    read_data('datasets/nov_2020.csv'),
-    read_data('datasets/first_quarter.csv'),
-    read_data('datasets/second_quarter.csv'),
-    read_data('datasets/third_quarter.csv')
-]
-fig.add_trace(
-    go.Bar(
-        x=publish_dates[1:],
-        y=[bankruptcy_data[i][1][2] for i in range(1, 5)],
-        name='%age of businesses bankrupt in 1-3 months'
+    months_until = 1: businesses that will go bankrupt in less than 1 month
+    months_until = 3: Businesses that will go bankrupt in 1 - 3 months
+    months_until = 6: Businesses that will go bankrupt in 3 - 6 months
+    months_until = 12: Businesses that will go bankrupt in 6 - 12 months
+    months_until = 999: Businesses that will go bankrupt in more than 12 months
+
+    Preconditions:
+    - months_until in {1, 3, 6, 12, 999}
+    """
+    values = [1, 3, 6, 12, 999]
+    indices = [0, 1, 2, 3, 4]
+    num_months = ['less than 1', '1 - 3', '3 - 6', '6 - 12', 'more than 12']
+    index = values.index(months_until)
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Bar(
+            x=PUBLISH_DATES[1:],
+            y=[get_percentage_for_quarter(i) for i in range(1, 5)],
+            name='%age of positive articles'
+        ))
+
+    bankruptcy_data = [
+        read_data('datasets/july_2020.csv'),
+        read_data('datasets/nov_2020.csv'),
+        read_data('datasets/first_quarter.csv'),
+        read_data('datasets/second_quarter.csv'),
+        read_data('datasets/third_quarter.csv')
+    ]
+    fig.add_trace(
+        go.Bar(
+            x=PUBLISH_DATES[1:],
+            # Change this last index to change the company size. Right now it's set at
+            # 20 - 99 employees.
+            y=[bankruptcy_data[i][indices[index]][2] for i in range(1, 5)],
+            name=f'%age of businesses bankrupt in {num_months[index]} month(s)'
+        )
     )
-)
 
-fig.update_layout(
-    title='Positive Media Representation vs Time until Bankruptcy',
-    yaxis = {
-        'title': 'Percentage'
-    }
-)
+    fig.update_layout(
+        title='Positive Media Representation vs Time until Bankruptcy',
+        yaxis={
+            'title': 'Percentage'
+        }
+    )
 
-fig.write_image('graph.png', format='png')
+    fig.write_image(f'graph{months_until}.png', format='png')
 
+
+if __name__ == '__main__':
+    import python_ta
+    import python_ta.contracts
+    python_ta.contracts.DEBUG_CONTRACTS = False
+    python_ta.contracts.check_all_contracts()
+    python_ta.check_all(config={
+        'extra-imports': [
+            'python_ta.contracts',
+            'kaleido', 'csv',
+            'plotly.graph_objects',
+            'datetime',
+            'python_ta',
+            'filtration'
+        ],
+        'allowed-io': ['plotly.graph_objects.Figure.write_image', 'with', 'open'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
